b892988962cf0ff20aed6a4219b2068f49ae47c4
Hanna L <hannalee92@gmail.com>
2022-05-01 13:21:57 -0400

    css

--- src/template.js
+++ src/template.js
@@ -2,11 +2,12 @@ const Employee = require("../lib/Employee");
 
 //Sub functions inside main function for every employee type, within each sub function return HTML, return statement use template literal
 const generateTeam = (team) => {
+
   const generateManager = (manager) => {
     return `<div class="card mx-auto" style="width: 50%">
         
         <div class="container">
-            <h4 class="card-header">Hanna L.</h4>
+            <h4 class="card-header">Manager</h4>
 
         <ul class="list-group list-group-flush">
             <li class="list-group-item">Name: ${manager.name}</li>
@@ -14,9 +15,12 @@ const generateTeam = (team) => {
             <li class="list-group-item">ID: ${manager.id}</li>
             <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
         </ul>
+
         </div>
+
     </div>`;
   };
+
   const generateIntern = (intern) => {
     return `<div class="card mx-auto" style="width: 50%">
         
@@ -29,13 +33,15 @@ const generateTeam = (team) => {
             <li class="list-group-item">ID: ${intern.id}</li>
             <li class="list-group-item">School: ${intern.school}</li>
         </ul>
+
         </div>
+
     </div>`;
   };
 
   const generateEngineer = (engineer) => {
     return `<div class="card mx-auto" style="width: 50%">
-        
+    
         <div class="container">
             <h4 class="card-header">Engineer</h4>
 
@@ -45,9 +51,12 @@ const generateTeam = (team) => {
             <li class="list-group-item">ID: ${engineer.id}</li>
             <li class="list-group-item">GitHub: <a target="_blank" href="https://github.com/${engineer.github}"> ${engineer.github} </a></li>
         </ul>
+
         </div>
+
     </div>`;
   };
+
   const html = [...team];
 
   for (let i = 0; i < html.length; i++) {
@@ -70,7 +79,7 @@ const generateTeam = (team) => {
 };
 
 //export function to generate page
-//module.export and arrow function, takes parameter of the team and returns the raw HTML of the whole HTML document
+//module.export and arrow function, takes parameter of the team and returns the HTML of the whole HTML document
 module.exports = (team) => {
   return `
     <!DOCTYPE html>
@@ -80,6 +89,7 @@ module.exports = (team) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="./Team-Profile-Generator/css/stylesheet.css">
         <title>Meet The Team</title>
     </head>
     <body>
@@ -87,7 +97,8 @@ module.exports = (team) => {
         <header>
             <h1>Meet The Team</h1>
         </header>
-        <main> ${generateTeam(team)} 
+        <main> 
+        ${generateTeam(team)} 
 
         </main>
 
@@ -95,4 +106,4 @@ module.exports = (team) => {
     </html> `;
 };
 
-//return HTML at the end
+//HTML at the end
